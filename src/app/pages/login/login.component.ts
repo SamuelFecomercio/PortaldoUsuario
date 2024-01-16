deb7f2d1cd0191065bdbdf128f5dd91c6e9e68e6
SamuelFecomercio <156712738+SamuelFecomercio@users.noreply.github.com>
2024-01-16 17:01:34 -0300

    add css

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from '../../componentes/login/login-component/login-component.component';
+import { LogoComponentComponent } from '../../componentes/login/logo-component/logo-component.component';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ LoginComponent ],
+  imports: [ LoginComponent, LogoComponentComponent ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
