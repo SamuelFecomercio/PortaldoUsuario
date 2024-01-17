2fc80d0d1b8695a5c21f447f75214660197d911e
SamuelFecomercio <156712738+SamuelFecomercio@users.noreply.github.com>
2024-01-17 13:43:04 -0300

    add CFP mask

--- src/app/app.modules.ts
+++ src/app/app.modules.ts
@@ -3,6 +3,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 
+
 @NgModule({
   imports: [
     MatFormFieldModule,
